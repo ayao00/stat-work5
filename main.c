857851c333e7183e84f85942c1dd9732c9533a54
Alvin Yao <ayao00@stuy.edu>
2019-11-13 22:24:19 -0500

    done

--- main.c
+++ main.c
@@ -5,16 +5,30 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+void printpermissions(mode_t mode){
+  char permissions[10] = "rwxrwxrwx";
+  for(int i = 0; i < 9; i++){
+    if(!(mode & 1<<(8-i))){
+      printf("-");
+    }else{
+      printf("%c", permissions[i]);
+    }
+  }
+  printf("\n");
+}
+
 int main(){
   struct stat test;
 
   stat("main.c", &test);
   printf("file size: %lld\n", test.st_size);
   printf("mode: %o\n", test.st_mode);
-  printf("%s\n", ctime(&test.st_atime));
+  printf("accessed: %s\n", ctime(&test.st_atime));
 
   char buffer[100];
   sprintf(buffer, "%lld B\t%lld KB\t%lld MB\t%lld GB", test.st_size, test.st_size/1000, test.st_size/1000000, test.st_size/1000000000);
 
   printf("%s\n", buffer);
+
+  printpermissions(test.st_mode);
 }
